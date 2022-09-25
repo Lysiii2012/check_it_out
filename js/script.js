16199c5a66a1053516d19bfc8fe203e524ed8705
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2022-09-25 17:16:30 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$( document ).ready(function() {
+    $(function() {
+        $('.close svg').click(function(){
+            $('.header_toasts').hide(5000);
+        });
+    });  
+
+    $(function() {
+        $('#nav-icon').click(function(){
+            $(this).toggleClass('open');
+            $('nav').toggleClass('show');
+        });
+    });
+    /**mob */
+    $(function() {
+        $('.btn_tel').click(function(){
+            $(this).toggleClass('active');
+            $('.hover_tell').toggleClass('show_tel');
+        });
+    });
+
+
+    /**dec */
+    $(function() {
+        $('.btn_tell').click(function(){
+            $(this).toggleClass('active');
+            $('.hover_tel').toggleClass('show_tell');
+        });
+    });
+
+
+    $(function() {
+        $('.support>h5').click(function(){
+            $(this).toggleClass('active');
+            $('.support>.about_use').toggleClass('show_info');
+        });
+    });
+    $(function() {
+        $('.clients>h5').click(function(){
+            $(this).toggleClass('active');
+            $('.clients>.about_use').toggleClass('show_info');
+        });
+    });
+
+    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav: true,
+        margin:0,
+        autoWidth:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    $('body').append(log);
+    $('[type*="radio"]').change(function () {
+        var me = $(this);
+        log.html(me.attr('value'));
+    });
+
+});
+
